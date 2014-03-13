7892cd9f351800375375ecbecd8064c72b4a01d8
tadasalisauskas <tadas@alisauskas.net>
2014-03-13 13:12:47 +0200

    Updated CPU register class

--- src/os1/CPU/CPU.java
+++ src/os1/CPU/CPU.java
@@ -27,6 +27,7 @@ public class CPU {
 		
 		this.IP = new Register2B();
 		this.SP = new Register2B();
+		
 		this.TIMER = new TimerRegister();
 		
 		this.C = new LogicalRegister();
@@ -37,6 +38,64 @@ public class CPU {
 		this.SI = InterruptRegister(0);
 		this.IOI = InterruptRegister(0);
 		this.TI = InterruptRegister(0);
+	}
+	
+	@Override
+	public String toString() {
+		String info = 
+				"";
+		return info;
+	}
+	
+// GETTERS AND SETTERS OF 4 BYTE REGISTERS
+	
+	public int getAX() {
+		return this.AX.getValue();
+	}
+	
+	public void setAX(int value) {
+		this.AX.setValue(value);
+	}
+	
+	public int getBX() {
+		return this.BX.getValue();
+	}
+	
+	public void setBX(int value) {
+		this.BX.setValue(value);
+	}
+	
+	public int getPTR() {
+		return this.PTR.getValue();
+	}
+	
+	public void setPTR(int value) {
+		this.PTR.setValue(value);
+	}
+	
+	
+// GETTERS AND SETTERS OF 2 BYTE REGISTERS
+	
+	public short getIP() {
+		return this.IP.getValue();
+	}
+	
+	public void setIP(short value) {
+		this.IP.setValue(value);
+	}
+	
+	public short getSP() {
+		return this.SP.getValue();
+	}
+	
+	public void setSP(short value) {
+		this.SP.setValue(value);
+	}
+	
+// GETTER AND SETTER OF TIMER REGISTER
+	
+	public TimerRegister getTIMER() {
+		return this.TIMER;
 	}
 	
 }
