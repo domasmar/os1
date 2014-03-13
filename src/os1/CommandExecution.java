7acc0bfdea97330f1de6cc51f5b7c20190c2fb32
ArthurasJ <jankus.arturas@gmail.com>
2014-03-13 18:48:46 +0200

    test

---
+++ src/os1/CommandExecution.java
@@ -0,0 +1,81 @@
+package os1;
+
+/**
+ *
+ * @author Arturas
+ */
+public class CommandExecution {
+
+    CommandExecution() {
+        
+    }
+    
+    public void execute(){
+    
+    }
+
+    public void cmdMov() {
+
+    }
+
+    public void cmdLoa() {
+
+    }
+
+    public void cmdSto() {
+
+    }
+
+    public void cmdPush() {
+
+    }
+
+    public void cmdPop() {
+
+    }
+
+    public void cmdAdd() {
+
+    }
+
+    public void cmdSub() {
+
+    }
+
+    public void cmdCmp() {
+
+    }
+
+    public void cmdJa() {
+
+    }
+
+    public void cmdJb() {
+
+    }
+
+    public void cmdJe() {
+
+    }
+
+    public void cmdJne() {
+
+    }
+
+    public void cmdOutrAx() {
+
+    }
+
+    public void cmdOutrxBx() {
+
+    }
+
+    public void cmdOutM() {
+
+    }
+    
+    public void cmdStop() {
+        
+    }
+
+}
--- src/os1/Memory/VMMemory.java
+++ src/os1/Memory/VMMemory.java
@@ -13,18 +13,28 @@ public class VMMemory {
 	}
 
 	public int getValue(int index) {
-		if (index / 16 <= cpu.getPTR() / (16*16)) {
-			int ptrA2 = cpu.getPTR() / 16 % 16; 
-			int ptrA3 = cpu.getPTR() % 16;							
-			int realBlock =  memory.getValue((16 * ptrA2 + ptrA3) * 16 + index / 16);
-			return memory.getValue(realBlock * 10 + index % 16);			
+		if (index / 16 <= cpu.getPTR() / (16 * 16)) {
+			int ptrA2 = cpu.getPTR() / 16 % 16;
+			int ptrA3 = cpu.getPTR() % 16;
+			int realBlock = memory.getValue((16 * ptrA2 + ptrA3) * 16 + index
+					/ 16);
+			return memory.getValue(realBlock * 10 + index % 16);
+		} else {
+			cpu.setPI((byte) 1);
+			throw new RuntimeException("PI = 1");
 		}
-		return 0;
 	}
 
 	public void setValue(int index, int value) {
-		if (index / 16 <= cpu.getPTR() / (16*16)) {
-			
+		if (index / 16 <= cpu.getPTR() / (16 * 16)) {
+			int ptrA2 = cpu.getPTR() / 16 % 16;
+			int ptrA3 = cpu.getPTR() % 16;
+			int realBlock = memory.getValue((16 * ptrA2 + ptrA3) * 16 + index
+					/ 16);
+			memory.setValue(realBlock * 10 + index % 16, value);
+		} else {
+			cpu.setPI((byte) 1);
+			throw new RuntimeException("PI = 1");
 		}
 	}
 
