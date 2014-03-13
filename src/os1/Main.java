1813b788269810c6b8407d6696e25e8cd0d52473
ArthurasJ <jankus.arturas@gmail.com>
2014-03-13 18:53:48 +0200

    Correcting main.java

--- src/os1/Main.java
+++ src/os1/Main.java
@@ -3,19 +3,14 @@ package os1;
 import java.util.Random;
 
 import os1.CPU.CPU;
-<<<<<<< HEAD
 import os1.Interpreter.Interpreter;
-=======
-import os1.Interpreter.CommandInterpreter;
->>>>>>> af75d1869dfa6d5f191216555ed53c70b220a08a
+import os1.Interpreter.Interpreter;
 import os1.Memory.RMMemory;
 import os1.Memory.VMMemory;
 
 public class Main {
 
 	public static void main(String args[]) {
-
-<<<<<<< HEAD
 		try { 
 			Interpreter ci = new Interpreter("mov bx, 80");
 			System.out.println(ci);
@@ -23,9 +18,7 @@ public class Main {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
-
-=======
->>>>>>> af75d1869dfa6d5f191216555ed53c70b220a08a
+                
 		CPU cpu = new CPU();
 		RMMemory rmMem = new RMMemory(cpu);
 		VMMemory vmMem = rmMem.createVMMemory(16);
