65b693af8012601f0a5100535f86a52f437411e2
tadasalisauskas <tadas@alisauskas.net>
2014-03-18 17:03:34 +0200

    Included labels recognizer

--- src/os1/CommandsConverter/CommandsConverter.java
+++ src/os1/CommandsConverter/CommandsConverter.java
@@ -3,6 +3,8 @@ package os1.CommandsConverter;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import os1.CommandsConverter.*;
+
 public class CommandsConverter {
 	
 	private static String[] machineCommands = {
@@ -17,6 +19,7 @@ public class CommandsConverter {
 		"ADD",
 		"SUB",
 		"CMP",
+		"JUMP",
 		"JA",
 		"JB",
 		"JE",
@@ -28,40 +31,49 @@ public class CommandsConverter {
 		"STOP"
 	};
 	
-	private HashMap<String, Integer> data = new HashMap<String, Integer>();	
+	private HashMap<String, Integer> variables = new HashMap<String, Integer>();
+	private HashMap<String, Integer> labels = new HashMap<String, Integer>();
 	private ArrayList<String> commands = new ArrayList<String>();
 	
 	private String[] sourceCode;
 	
-	private static int countLines(String str) {
-		String[] lines = str.split("\\r?\\n");
-		return lines.length;
-	}
-	
 	public CommandsConverter(String sourceCode) {
-		int lines = countLines(sourceCode);
-		this.sourceCode = new String[lines];
-		this.sourceCode = sourceCode.split("\n");
+		saveSourceCode(sourceCode);
 		saveCommands();
 		saveVariables();
+		saveLabels(findJumpVariables());
 	}
 	
 	public String[] getSourceCode() {
 		return this.sourceCode;
 	}
 	
-	public HashMap<String, Integer> getData() {
-		return this.data;
+	public HashMap<String, Integer> getVariables() {
+		return this.variables;
+	}
+	
+	/* Ið paduoto pirminio kodo teksto paðalina visas tuðèias eilutes ir tarpus bei á masyvà "sourceCode" áraðo kiekvienà pirminio kodo eilutæ */
+	private void saveSourceCode(String sourceCode) {
+		String tidySourceCode = sourceCode.replaceAll("(?m)^[ \t]*\r?\n", "").trim();
+		String[] tempSourceCode = tidySourceCode.split("\n");
+		this.sourceCode = new String[tempSourceCode.length + 1];
+		for (int i = (tempSourceCode.length - 1); i >= 0; i--) {
+			this.sourceCode[i + 1] = tempSourceCode[i];
+		}
 	}
 	
 	public String[] getCommands() {
-		String[] commandsArray = this.commands.toArray(new String[this.commands.size()]);
+		String[] tempCommandsArray = this.commands.toArray(new String[this.commands.size()]);
+		String[] commandsArray = new String[tempCommandsArray.length + 1];
+		for (int i = (tempCommandsArray.length - 1); i >= 0; i--) {
+			commandsArray[i + 1] = tempCommandsArray[i];
+		}
 		return commandsArray;
 	}
 	
 	/* Á sàraðà iðsaugo tik tas pirminio kodo eilutes, kuriose yra komandos */
 	private void saveCommands() {
-		for (int i = 0; i <= this.sourceCode.length - 1; i++) {
+		for (int i = 1; i <= this.sourceCode.length - 1; i++) {
 			for (int j = 0; j < machineCommands.length - 1; j++) {
 				if (sourceCode[i].contains(machineCommands[j])) {
 					this.commands.add(this.sourceCode[i]);
@@ -70,12 +82,47 @@ public class CommandsConverter {
 		}
 	}
 	
-	/* Á HashMap iðsaugomos kintamøjø reikðmës ir patys kintamieji (pvz.: this.data.get("var1") = FFFF) */
+	/* Á HashMap iðsaugomi kintamieji ir jø reikðmës (pvz.: this.data.get("var1") = FFFF) */
 	private void saveVariables() {
-		for (int i = 0; i <= this.sourceCode.length - 1; i++) {
+		for (int i = 1; i <= this.sourceCode.length - 1; i++) {
 			if (this.sourceCode[i].contains("DEF")) {
 				String[] definition = this.sourceCode[i].split(" DEF ");
-				this.data.put(definition[0], Integer.parseInt(definition[1], 16));
+				this.variables.put(definition[0], Integer.parseInt(definition[1], 16));
+			}
+		}
+//		for (String key: variables.keySet()) {
+//		    System.out.println(key);
+//		}
+//		
+//		for (int value: variables.values()) {
+//			System.out.println(value);
+//		}
+	}
+	
+	/* Á sàraðà sudedami visi JUMP, JA, JB, JE komandø rasti kintamieji */
+	private ArrayList<String> findJumpVariables() {
+		ArrayList<String> variables = new ArrayList<String>();
+		for (int i = 1; i <= this.sourceCode.length - 1; i++) {
+			if (this.sourceCode[i].contains("JUMP") ||
+					this.sourceCode[i].contains("JA") ||
+					this.sourceCode[i].contains("JB") ||
+					this.sourceCode[i].contains("JE")) {
+				variables.add(this.sourceCode[i].split(" ")[1]);
+			}
+		}
+		return variables;
+	}
+	
+	private void saveLabels(ArrayList<String> labels) {
+		for (int i = 1; i <= this.sourceCode.length - 1; i++) {
+			for (int j = 0; j <= labels.size() - 1; j++) {
+				if (this.sourceCode[i].trim().equalsIgnoreCase(labels.get(j))) {
+					for (int k = 0; k <= this.commands.size() - 1; k++) {
+						if (this.commands.get(k).equalsIgnoreCase(this.sourceCode[i + 1])) {
+							this.labels.put(labels.get(j), k + 1);
+						}
+					}
+				}
 			}
 		}
 	}
--- src/os1/MainTadas.java
+++ src/os1/MainTadas.java
@@ -10,33 +10,46 @@ public class MainTadas {
 				"var1 DEF FF\n"
 			  + "var2 DEF EE\n"
 			  + "var3 DEF F0\n"
-			  + "priskyrimas\n"
+			  + "var4 DEF AA\n"
+			  + "\n"
+			  + "\n"
+			  + "sudetis\n"
 			  + "MOV_AX var1\n"
 			  + "MOV_BX var2\n"
 			  + "PUSH AX\n"
 			  + "PUSH BX\n"
 			  + "ADD\n"
-			  + "JUMP priskyrimas";
+			  + "JUMP sudetis\n"
+			  + "\n"
+			  + "\n"
+			  + "atimtis\n"
+			  + "MOV_AX var3\n"
+			  + "MOV_BX var4\n"
+			  + "SUB\n"
+			  + "JUMP atimtis";
 		
 		CommandsConverter cc = new CommandsConverter(sourceCode);
 		
 		/* Visas programos pirminis kodas */
-		for(int i = 0; i <= cc.getSourceCode().length - 1; i++) {
-			System.out.println(i + ". " + cc.getSourceCode()[i]);
+		System.out.println("PIRMINIS KODAS:");
+		for(int i = 1; i <= cc.getSourceCode().length - 1; i++) {
+			System.out.println(i + 1 + ". " + cc.getSourceCode()[i]);
 		}
 		
 		System.out.println();
 		
 		/* Visos programos pirminio kodo komandos */
-		for(int i = 0; i <= cc.getCommands().length - 1; i++) {
+		System.out.println("KOMANDOS:");
+		for(int i = 1; i <= cc.getCommands().length - 1; i++) {
 			System.out.println(i + ". " + cc.getCommands()[i]);
 		}
 		
 		System.out.println();
 		
 		/* Kintamøjø reikðmës */
-		for(int i = 1; i <= cc.getData().size(); i++) {
-			System.out.println(cc.getData().get("var" + i));
+		System.out.println("KINTAMIEJI IR JØ REIKÐMËS:");
+		for(int i = 1; i <= cc.getVariables().size(); i++) {
+			System.out.println("var" + i + ": " + cc.getVariables().get("var" + i));
 		}
 	}
 
