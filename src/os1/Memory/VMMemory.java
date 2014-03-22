5bfb0f084b22b7a4e74888a42217eabb90818dc7
OrangeCup <domas933@gmail.com>
2014-03-22 18:31:19 +0200

    Fixed bug in VMMemory addressation

--- src/os1/Memory/VMMemory.java
+++ src/os1/Memory/VMMemory.java
@@ -14,13 +14,18 @@ public class VMMemory {
 		this.setSize(size);
 	}
 
+	private int getRealAddress(int index) {
+		int ptrA2 = cpu.getPTR() / 16 % 16;
+		int ptrA3 = cpu.getPTR() % 16;
+		int realBlock = memory.getValue((16 * ptrA2 + ptrA3) * 16 + index / 16);
+
+		return realBlock * 16 + index % 16;
+	}
+
 	public int getValue(int index) {
 		if (index / 16 <= cpu.getPTR() / (16 * 16)) {
-			int ptrA2 = cpu.getPTR() / 16 % 16;
-			int ptrA3 = cpu.getPTR() % 16;
-			int realBlock = memory.getValue((16 * ptrA2 + ptrA3) * 16 + index
-					/ 16);
-			return memory.getValue(realBlock * 10 + index % 16);
+			//System.out.println("Virtualus: " + index + " Realus: " + getRealAddress(index));
+			return memory.getValue(getRealAddress(index));
 		} else {
 			cpu.setPI((byte) 1);
 			throw new RuntimeException("PI = 1");
@@ -29,11 +34,7 @@ public class VMMemory {
 
 	public void setValue(int index, int value) {
 		if (index / 16 <= cpu.getPTR() / (16 * 16)) {
-			int ptrA2 = cpu.getPTR() / 16 % 16;
-			int ptrA3 = cpu.getPTR() % 16;
-			int realBlock = memory.getValue((16 * ptrA2 + ptrA3) * 16 + index
-					/ 16);
-			memory.setValue(realBlock * 10 + index % 16, value);
+			memory.setValue(getRealAddress(index), value);
 		} else {
 			cpu.setPI((byte) 1);
 			throw new RuntimeException("PI = 1");
