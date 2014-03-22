f4ccf82156a5185e25dfdcf545781b866c62325e
OrangeCup <domas933@gmail.com>
2014-03-22 19:43:30 +0200

    Added command JMP

--- src/os1/Interpreter/Command.java
+++ src/os1/Interpreter/Command.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package os1.Interpreter;
 
 public enum Command {
@@ -25,31 +24,3 @@ public enum Command {
 	// FORK
 	STOP,
 }
-=======
-package os1.Interpreter;
-
-public enum Command {
-	
-        MOV_AX,// 4 bytes -> register AX
-        MOV_BX,// 4 bytes -> register BX
-	LOA_AX,// memory -> register
-	LOA_BX,
-	STO_AX, // register -> memory
-	STO_BX,
-	PUSH, 
-	POP,
-	ADD, // 2 viršutinių elementų suma -> AX
-	SUB, // 2 viršutinių elementų skirtumas -> AX
-	CMP,
-        JMP,
-	JA, // jump if above
-	JB, // jump if below
-	JE, // jump if equal
-	JNE, // jump if not equa
-	OUTR_AX, // prints register value
-	OUTR_BX,
-	OUTM, // prints memory value 
-	// FORK
-	STOP,
-}
->>>>>>> 5d7dc73afe06a01240f98c5f45538fdb1594f7a1
