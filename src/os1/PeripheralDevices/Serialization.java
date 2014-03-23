96bf3f509556369404e53ef75492144bec6a1eaa
ArthurasJ <jankus.arturas@gmail.com>
2014-03-23 16:04:03 +0200

    Created class for serializing HDD object.

---
+++ src/os1/PeripheralDevices/Serialization.java
@@ -0,0 +1,45 @@
+package os1.PeripheralDevices;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ *
+ * @author Arturas
+ */
+public class Serialization {
+
+    public static void serializeMemory(HDD memory) {
+        try {
+            FileOutputStream fileOut = new FileOutputStream("HDD");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(memory);
+            out.close();
+            fileOut.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
+
+    public static HDD deserializeMemory() {
+        HDD memory = null;
+        try {
+            FileInputStream fileIn = new FileInputStream("HDD");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            memory = (HDD) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+            return null;
+        } catch (ClassNotFoundException c) {
+            c.printStackTrace();
+            return null;
+        }
+        return memory;
+    }
+
+}
