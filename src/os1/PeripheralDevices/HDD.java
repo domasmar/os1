859bc338d7229c5ff8197fe720ba131bbbbb5b91
ArthurasJ <jankus.arturas@gmail.com>
2014-03-23 17:02:26 +0200

    HDD created.

--- src/os1/PeripheralDevices/HDD.java
+++ src/os1/PeripheralDevices/HDD.java
@@ -1,6 +1,6 @@
 package os1.PeripheralDevices;
 
-import java.io.Serializable;
+import java.io.*;
 
 /**
  *
@@ -8,4 +8,50 @@ import java.io.Serializable;
  */
 public class HDD implements Serializable {
 
+    private final int maxSize = 100;
+
+    private SourceFile[] memory = new SourceFile[maxSize];
+
+    public void store(SourceFile file) throws Exception {
+        for (int i = 0; i < maxSize; i++) {
+            if (memory[i] == null) {
+                memory[i] = file;
+                return;
+            }
+        }
+        throw new Exception("Disk is full!");
+    }
+
+    public SourceFile load(String name) throws UnsupportedEncodingException, Exception {
+        for (int i = 0; i < maxSize; i++) {
+            if ((memory[i] != null) && (name.equals(memory[i].getFileName()))) {
+                return memory[i];
+            }
+        }
+        throw new Exception("File not found!");
+    }
+
+    public void delete(String name) throws UnsupportedEncodingException, Exception {
+        for (int i = 0; i < maxSize; i++) {
+            if ((memory[i] != null) && (name.equals(memory[i].getFileName()))) {
+                memory[i] = null;
+                return;
+            }
+        }
+        throw new Exception("File not found!");
+    }
+
+    public String[] getFilesList() throws UnsupportedEncodingException {
+        String[] list = new String[maxSize];
+        int j = 0;
+        for (int i = 0; i < maxSize; i++) {
+            if (memory[i] != null) {
+                list[j] = memory[i].getFileName();
+                j++;
+            }
+        }
+        String[] shortenedList = new String[j];
+        System.arraycopy(list, 0, shortenedList, 0, j);
+        return shortenedList;
+    }
 }
